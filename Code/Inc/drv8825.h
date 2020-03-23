a4837d9408ed7f5e3ea644fdc1abce460752d5b1
G0dd4 <40282122+G0dd4@users.noreply.github.com>
2020-03-23 12:18:21 +0100

    Add files via upload

---
+++ Code/Inc/drv8825.h
@@ -0,0 +1,31 @@
+#ifndef DRV8825_H
+#define DRV8825_H
+
+#include "mbed.h"
+#include "platform/mbed_thread.h"
+
+#define FORWARD 1
+#define BACKWARD 0
+
+#define START 0
+#define STOP 1
+
+class DRV8825{
+    public:
+    DRV8825();
+    DRV8825(PinName  pinEN, PinName pinDIR, PinName pinStep);
+
+    void setDir(uint8_t dir);
+    void setEnable(uint8_t en);
+    void move(float Frequency);
+
+
+    private:
+    DigitalOut* m_en;
+    DigitalOut* m_dir;
+    PwmOut* m_step;
+
+};
+
+
+#endif DRV8825_H //DRV8825_H
