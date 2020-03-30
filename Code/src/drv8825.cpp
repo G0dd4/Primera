a4d75e296e299d427692a47a51a184dc39c3d6bd
G0dd4 <40282122+G0dd4@users.noreply.github.com>
2020-03-30 09:28:24 +0200

    Add files via upload

--- Code/src/drv8825.cpp
+++ Code/src/drv8825.cpp
@@ -2,9 +2,9 @@
 
 
 DRV8825::DRV8825(){
-    m_en = new DigitalOut(D7);
-    m_dir = new DigitalOut(D5);
-    m_step = new PwmOut(D6);
+    m_en = new DigitalOut(PI_3);
+    m_dir = new DigitalOut(PI_0);
+    m_step = new FastPWM(PH_6);
     m_step->write(0.50);
 }
 
@@ -12,7 +12,7 @@ DRV8825::DRV8825(){
 DRV8825::DRV8825(PinName en, PinName dir ,PinName step){
     m_en = new DigitalOut(en);
     m_dir = new DigitalOut(dir);
-    m_step = new PwmOut(step);
+    m_step = new FastPWM(step);
     m_step->write(0.50);
 }
 
@@ -24,6 +24,7 @@ void DRV8825::setEnable(uint8_t en){
         m_en->write(en);
 }
 
-void DRV8825::move(float frequency){
-        m_step->period((float)1/frequency);
+void DRV8825::move(unsigned int frequency){
+        m_step->period_ms(frequency);
+        m_step->write(0.50);
 }
