d65a19e6cd705e067ae687150864b507aaaffa5d
G0dd4 <yannis.aissaoui@hotmail.fr>
2020-04-18 13:44:34 +0200

    modification de interface.h

--- Code/Inc/interface.h
+++ Code/Inc/interface.h
@@ -3,12 +3,12 @@
 
 #include "progView.h"
 #include "LabiView.h"
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h> //provisoir (simulation PC)
 #include "lvgl/lvgl.h"
-#include "lv_drivers/display/monitor.h"
-#include "lv_drivers/indev/mouse.h"
-#include "lv_drivers/indev/mousewheel.h"
-#include "lv_drivers/indev/keyboard.h"
+#include "lv_drivers/display/monitor.h" //provisoir (simulation PC)
+#include "lv_drivers/indev/mouse.h" //provisoir (simulation PC)
+#include "lv_drivers/indev/mousewheel.h" //provisoir (simulation PC)
+#include "lv_drivers/indev/keyboard.h" //provisoir (simulation PC)
 
 static lv_obj_t* m_calc;
 static lv_obj_t* m_labi;
