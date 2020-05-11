af7e30c909cd52d836fc4e025eac46fe9696ad15
G0dd4 <yannis.aissaoui@hotmail.fr>
2020-05-11 09:30:19 +0200

    Création de la view calcul

---
+++ Code/Inc/calcView.h
@@ -0,0 +1,18 @@
+#ifndef CALCVIEW_H
+#define CALCVIEW_H
+#include "lvgl/lvgl.h"
+
+
+/**************************
+ * Création de la fenêtre *
+ **************************/
+void creatCalc(lv_obj_t* m_calc);
+void styleCalc(lv_obj_t* m_calc);
+
+
+/*******************
+ * différent event *
+ *******************/
+
+
+#endif // CALCVIEW_H
